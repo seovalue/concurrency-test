380f26eba7290a75c1e26018344b43a7d55c8d97
seovalue <seominjeong.dev@gmail.com>
2021-12-24 11:40:50 +0900

    etc: initial commit

---
+++ src/test/java/seovalue/concurrency/ConcurrencyApplicationTests.java
@@ -0,0 +1,13 @@
+package seovalue.concurrency;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConcurrencyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
